863e80ce5ee25ad7a1cf9ba468f80c96df0a86cf
Uthoai Marma <uthoaimarma597@gmail.com>
2025-02-21 21:53:56 +0600

    foreground service

--- app/src/main/java/com/services/MainActivity.kt
+++ app/src/main/java/com/services/MainActivity.kt
@@ -1,22 +1,33 @@
 package com.services
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.services.databinding.ActivityMainBinding
 import com.services.service.LoggerService
 
+@RequiresApi(Build.VERSION_CODES.TIRAMISU)
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.POST_NOTIFICATIONS),
+                100)
+        }
 
         binding.apply {
             btnStartService.setOnClickListener {
-                startService(Intent(this@MainActivity, LoggerService::class.java))
+                startForegroundService(Intent(this@MainActivity, LoggerService::class.java))
             }
             btnStopService.setOnClickListener {
                 stopService(Intent(this@MainActivity, LoggerService::class.java))
--- app/src/main/java/com/services/service/LoggerService.kt
+++ app/src/main/java/com/services/service/LoggerService.kt
@@ -1,11 +1,20 @@
 package com.services.service
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
+import android.os.Build
 import android.os.IBinder
 import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import com.services.MainActivity
+import com.services.R
 import kotlin.concurrent.thread
-import kotlin.math.truncate
 
 class LoggerService: Service() {
 
@@ -26,6 +35,8 @@ class LoggerService: Service() {
                 Log.d("ServiceTag", "onCreate: Service is Running")
             }
         }
+
+        startLoggerForegroundService()
         //stopSelf()
         return super.onStartCommand(intent, flags, startId)
     }
@@ -33,6 +44,54 @@ class LoggerService: Service() {
     override fun onDestroy() {
         Log.d("ServiceTag", "onCreate: Service Stop")
         super.onDestroy()
+    }
+
+
+    private fun startLoggerForegroundService(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            createNotificationChannel()
+            val notification = createNotification()
+            startForeground(1, notification)
+            Log.d("ServiceTag", "startForeground: Service is Running")
+        }
+        else{
+            startForeground(1, Notification())
+        }
+    }
+
+    // notification for foreground service
+    private fun getPendingIntent(): PendingIntent{
+        val notificationIntent = Intent(this, MainActivity::class.java)
+        return PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_IMMUTABLE)
+    }
+
+    private val channelId = "notification_channel_id"
+    private val channelName = "Notification Channel"
+    private val channelDescription = "Description of the notification channel"
+    private val importance = NotificationManager.IMPORTANCE_DEFAULT
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun createNotificationChannel(): NotificationChannel{
+        val channel = NotificationChannel(channelId, channelName, importance).apply {
+            description = channelDescription
+        }
+        // Register the channel with the system
+        /*val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)*/
+        val notificationManager: NotificationManager = ContextCompat.getSystemService(this, NotificationManager::class.java) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+        return channel
+    }
+
+    private fun createNotification(): Notification {
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setContentTitle("Foreground Service")
+            .setContentText("Service is running")
+            .setContentIntent(getPendingIntent())
+            .setSmallIcon(R.drawable.ic_pokemon_24)
+            .setOngoing(true)
+            .build()
+        return notification
     }
 
 }
